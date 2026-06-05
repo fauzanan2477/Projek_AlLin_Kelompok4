12058e4f59c714e7df3defb9227355e8a1e26cd0
nadhirarifai8-svg <nadhirarifai8@student.uns.ac.id>
2026-06-05 12:22:17 +0700

    tambah ke tengah

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown("""
      font-weight: 900; 
      color: #FFFFFF; 
      text-align: center; 
-     margin-top: 32vh; 
+     margin-top: 10vh; 
      line-height: 1.3;
  }
  .hero-title-large span { color: #EF8354; } 
